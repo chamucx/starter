c0731b4d0f652aa82ec0caa059bb805d9580df46
Antonio <joseantoniosysnet@gmail.com>
2023-12-21 11:24:15 -0500

    added files

---
+++ dog_shelters/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Shelter(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    #TO-DO Add the id for Shelter
+    id = models.BigAutoField(primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+class Dog(models.Model):
+    shelter = models.ForeignKey(Shelter, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    intake_date = models.DateTimeField(auto_now_add=True)
+    #TO-DO Add the id for Shelter
+    id = models.BigAutoField(primary_key=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('dog_detail', args=[str(self.id)])
---
+++ project/azure.py
@@ -0,0 +1,32 @@
+from .settings import *
+import os
+
+WIBBLE2 = 'Wibble2'
+
+
+
+
+# TO-DO ADD IT CSRF_TRUSTED_ORIGINS = ['https://*'] as wildcar locally
+# TO-DO once deployed from VS code
+# CSRF_TRUSTED_ORIGINS = ['https://youdomain.azurewebsites.net']
+
+CSRF_TRUSTED_ORIGINS = ['demoxdqw1.azurewebsites.net']
+
+
+
+# TO-ADD 
+#add the next middleware for whitenoise
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    # Enables whitenoise for serving static files
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+#TO-DO add the static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
